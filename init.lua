d5ea3a1e195866c0592ecd25327dcee71646a5df
hiki349 <vladyakima@gmail.com>
2024-03-16 00:17:50 +0200

    update config

--- init.lua
+++ init.lua
@@ -14,7 +14,11 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  'nyoom-engineering/oxocarbon.nvim',
+  {
+    'uloco/bluloco.nvim',
+     dependencies = { 'rktjmp/lush.nvim' },
+  },
+--  'ellisonleao/gruvbox.nvim',
   'neovim/nvim-lspconfig',
   'nvim-treesitter/nvim-treesitter',
   'nvim-tree/nvim-tree.lua',
@@ -32,9 +36,20 @@ require("lazy").setup({
     'neoclide/coc.nvim',
     branch = 'release'
   },
+  { 'codota/tabnine-nvim', build = "./dl_binaries.sh" },
+})
+require('tabnine').setup({
+  disable_auto_comment=true,
+  accept_keymap="<C-j>",
+  dismiss_keymap = "<C-]>",
+  debounce_ms = 800,
+  suggestion_color = {gui = "#808080", cterm = 244},
+  exclude_filetypes = {"TelescopePrompt", "NvimTree"},
+  log_file_path = nil, -- absolute path to Tabnine log file
 })
 
-vim.cmd.colorscheme "oxocarbon"
+-- vim.o.background = "dark"
+vim.cmd.colorscheme 'bluloco'
 
 local lspconfig = require('lspconfig')
 lspconfig.tsserver.setup {}
